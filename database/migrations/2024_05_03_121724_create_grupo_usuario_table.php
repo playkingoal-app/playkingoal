e5e3a1a58d086def292757594e5aa0ba1b69e567
playkingoal <playkingoal@gmail.com>
2026-06-02 20:19:40 +0200

    Corregir orden de migraciones 1.4

--- database/migrations/2024_05_03_121724_create_grupo_usuario_table.php
+++ database/migrations/2024_05_03_121724_create_grupo_usuario_table.php
@@ -14,6 +14,9 @@ return new class extends Migration {
     {
         Schema::create('grupo_usuario', function (Blueprint $table) {
             $table->id();
+            $table->bigInteger('grupo_id')->unsigned();
+            $table->bigInteger('usuario_id')->unsigned();
+
             $table->foreign('grupo_id')->references('id')->on('grupos')->onDelete("cascade");
             $table->foreign('usuario_id')->references('id')->on('users')->onDelete("cascade");
             $table->enum('rol', ['admin', 'jugador']);
