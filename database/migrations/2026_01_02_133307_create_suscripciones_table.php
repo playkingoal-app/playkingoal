356caf7ffc96f7194375f7aa16c61e0cf458a83e
playkingoal <playkingoal@gmail.com>
2026-06-02 20:22:31 +0200

    Corregir orden de migraciones 1.6

--- database/migrations/2026_01_02_133307_create_suscripciones_table.php
+++ database/migrations/2026_01_02_133307_create_suscripciones_table.php
@@ -4,25 +4,27 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
      * @return void
      */
-   public function up()
-{
-    Schema::create('suscripciones', function (Blueprint $table) {
-        $table->id();
-        $table->foreign('usuario_id')->references('id')->on('users')->onDelete("cascade");
-        $table->foreignId('plan_id')->references('id')->on('planes')->onDelete("cascade");    
-        $table->enum('estado', ['activa', 'cancelada', 'expirada'])->default('activa');
-        $table->date('inicia_en');
-        $table->date('vence_en');
-        $table->timestamps();
-    });
-}
+    public function up()
+    {
+        Schema::create('suscripciones', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('usuario_id')->unsigned();
+            $table->bigInteger('plan_id')->unsigned();
+
+            $table->foreign('usuario_id')->references('id')->on('users')->onDelete("cascade");
+            $table->foreignId('plan_id')->references('id')->on('planes')->onDelete("cascade");
+            $table->enum('estado', ['activa', 'cancelada', 'expirada'])->default('activa');
+            $table->date('inicia_en');
+            $table->date('vence_en');
+            $table->timestamps();
+        });
+    }
 
 
     /**
